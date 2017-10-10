74ceec8f45f946c69080e23ecafd557e43393007
João Marcelo Souza <joaomarcelo9393@gmail.com>
2017-10-10 09:03:47 -0300

    Dynamic chart crash

--- ChartRocks/ViewController.swift
+++ ChartRocks/ViewController.swift
@@ -6,20 +6,65 @@
 //  Copyright © 2017 João Marcelo Souza. All rights reserved.
 //
 
+import Charts
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chart: LineChartView!
+    var timer: Timer!
+    var offset: TimeInterval!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // TO SEE CRASH:
+        // Comment `setupStaticChart()` and uncomment `setupDynamicChart()` below.
+        
+        // Static chart works fine
+        setupStaticChart()
+        
+        // (!) Dynamic chart crashes
+        //setupDynamicChart()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupStaticChart() {
+        var dataEntries = [ChartDataEntry]()
+        for i in 1...10 {
+            let entry = ChartDataEntry(x: Double(i), y: Double(arc4random()))
+            dataEntries.append(entry)
+        }
+        let dataSet = LineChartDataSet(values: dataEntries, label: "Random Value")
+        let lineData = LineChartData(dataSet: dataSet)
+        chart.data = lineData
+        setupChartViewport()
     }
-
+    
+    func setupDynamicChart() {
+        offset = Date.timeIntervalSinceReferenceDate
+        let dataSet = LineChartDataSet(values: [], label: "Random Value")
+        let lineData = LineChartData(dataSet: dataSet)
+        chart.data = lineData
+        setupChartViewport()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (_) in
+            self.addRandomData()
+        })
+    }
+    
+    func addRandomData() {
+        let entry = ChartDataEntry(x: Date.timeIntervalSinceReferenceDate - offset, y: Double(arc4random()))
+        chart.data?.addEntry(entry, dataSetIndex: 0)
+        chart.notifyDataSetChanged()
+    }
+    
+    func setupChartViewport() {
+        // On dynamic chart, this shows nothing:
+         chart.moveViewToX(0)
+        
+        // On dynamic chart, this crashes:
+         chart.setVisibleXRangeMaximum(4)
+    }
 
 }
 
